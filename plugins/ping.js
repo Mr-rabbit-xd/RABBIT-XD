6650b41905ebb2fd7643795a2e488082c1e7ff2d
Mr-rabbit-xd <bihh5539@gmail.com>
2025-04-02 15:54:37 +0530

    ＭＲ－ Ｒａｂｂｉｔ

---
+++ plugins/ping.js
@@ -0,0 +1,44 @@
+import config from '../config.cjs';
+
+const ping = async (m, Matrix) => {
+  const prefix = config.PREFIX;
+  const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0].toLowerCase() : '';
+
+  if (cmd === "ping") {
+    const start = new Date().getTime();
+
+    const reactionEmojis = ['🔥', '⚡', '🚀', '💨', '🎯', '🎉', '🌟', '💥', '🕐', '🔹'];
+    const textEmojis = ['💎', '🏆', '⚡️', '🚀', '🎶', '🌠', '🌀', '🔱', '🛡️', '✨'];
+
+    const reactionEmoji = reactionEmojis[Math.floor(Math.random() * reactionEmojis.length)];
+    let textEmoji = textEmojis[Math.floor(Math.random() * textEmojis.length)];
+
+    // Ensure reaction and text emojis are different
+    while (textEmoji === reactionEmoji) {
+      textEmoji = textEmojis[Math.floor(Math.random() * textEmojis.length)];
+    }
+
+    await m.React(textEmoji);
+
+    const end = new Date().getTime();
+    const responseTime = (end - start) / 1000;
+
+    const text = `*Ｒａｂｂｉｔ SPEED: ${responseTime.toFixed(2)}ms ${reactionEmoji}*`;
+
+    await Matrix.sendMessage(m.from, {
+      text,
+      contextInfo: {
+        mentionedJid: [m.sender],
+        forwardingScore: 999,
+        isForwarded: true,
+        forwardedNewsletterMessageInfo: {
+          newsletterJid: '120363406899332269@newsletter',
+          newsletterName: "ＭＲ－ Ｒａｂｂｉｔ",
+          serverMessageId: 143
+        }
+      }
+    }, { quoted: m });
+  }
+};
+
+export default ping;
